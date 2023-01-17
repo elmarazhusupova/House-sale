12b6a5e4aaa5e94132190ef01e9e7b3a4e4b5de8
elmarazhusupova <zhusupovaelmara80@gmail.com>
2023-01-17 15:55:10 +0600

    hasbeen

--- account/forms.py
+++ account/forms.py
@@ -1,10 +1,46 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django import forms 
+# from django.forms import ModelForm
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from django import forms 
 
 
-class CreateUserForm(UserCreationForm):
+# class CreateUserForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+
+
+from django import forms
+from .models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Введите пароль')
+    password_confirmation = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Повторите пароль')
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'email', 'password', 'password_confirmation']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('User with such email already exists')
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = data.get('password')
+        password_confirm = data.pop('password_confirmation')
+        if password != password_confirm:
+            raise forms.ValidationError('Invalid password')
+        return data
+
+    def save(self, commit=True):
+        user = User.objects.create(**self.cleaned_data)
+        return user
+
+
+# class UserLoginForm(AuthenticationForm):
+#     username = forms.CharField(widget=forms.TextInput(),label='email')
+#     password = forms.CharField(widget=forms.PasswordInput(),label='username')
--- account/views.py
+++ account/views.py
@@ -1,24 +1,71 @@
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+# from .forms import CreateUserForm
+# from django.contrib import messages
+
+
+# def registerPage(request):
+#     form = CreateUserForm()
+
+#     if request.method == 'POST':
+#         form = CreateUserForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             user = form.cleaned_data.get('username')
+#             messages.success(request, 'Account was successfully created!')
+#             return redirect('login')
+
+#     context = {'form':form}
+#     return render(request, 'account/register.html', context)
+
+
+
+# def loginPage(request):
+#     context = {}
+#     return render(request, 'account/login.html', context)
+
+
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 
+def registerView(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
 
-def registerPage(request):
-    form = CreateUserForm()
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect(request, 'register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('index')
+        else:
+            messages.info(request, 'Password not Matching')
+            return redirect('register')
+    else:
+        return render(request, 'account/register.html')
+
+def loginView(request):
 
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Account was successfully created!')
-            return redirect('login')
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
 
-    context = {'form':form}
-    return render(request, 'account/register.html', context)
-
-
-
-def loginPage(request):
-    context = {}
-    return render(request, 'account/login.html', context)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('register')
+    else:
+        return render(request, 'account/register.html') 
